0286dc71379f6288fd859f994484f0e946651f19
Erland <nicolasjonason@gmail.com>
2019-02-20 13:58:45 +0100

    add guess

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,39 @@
 import React, { Component } from 'react';
-import qAPI from './qAPI/qAPI';
 import './App.css';
+import qAPI from './qAPI/qAPI';
 
 class App extends Component {
 	constructor() {
 		super();
 		this.api = new qAPI();
 		this.state = {
-			question:null
+			question:null, //bad name
+			guess:"",
+			answered:false
 		};
 		this.api.getQuestion(0).then(res => this.setState({ question:res }));
 	}
 
-	componentDidMount() {}
+	onEnter(){
+		this.setState({answered:true})
+	}
 
 	render() {
 
-		// let content="loading question";
-
-		// if(this.state.question){
-		// 	=this.state.question.q} ? unit:{this.state.question.unit}</h4>;
-		// }
-
 		return (
 			<React.Fragment>
-				<h1>STATBOY9z</h1>
-				<h2>{this.state.question?this.state.question.q:""}</h2>
-				<h3>{this.state.question?this.state.question.unit:""}</h3>
-				<input></input>
+				<h1>STATBOY</h1>
+				<h2 className="text-left">>{this.state.question?this.state.question.q:""}</h2>
+				<h3 className="text-left">({this.state.question?this.state.question.unit:""})</h3>
+				<input onKeyPress={(e)=>e.keyCode||e.charCode==13?this.onEnter():""}   onChange={e=>this.setState({guess:e.target.value})}></input>
+				<h3 className="text-center">{this.state.answered?this.state.question.a:"press enter"} </h3>
+				<h3 className="text-left">score:{this.state.answered?Math.log10(Math.abs((Number(this.state.question.a)-Number(this.state.guess)))):"---"} </h3>
+				
 			</React.Fragment>
 		);
 	}
 }
+
+
 
 export default App;
