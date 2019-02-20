ba49e4b7d454a054018c1eecbcf56a71703dd744
Erland <nicolasjonason@gmail.com>
2019-02-21 00:03:41 +0100

    various performance fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,38 +2,83 @@ import React, { Component } from 'react';
 import './App.css';
 import qAPI from './qAPI/qAPI';
 
+import QuestionHolder from './QuestionHolder/QuestionHolder';
+
 class App extends Component {
 	constructor() {
 		super();
 		this.api = new qAPI();
 		this.state = {
-			question:null, //bad name
-			guess:"",
-			answered:false
+			id: 0,
+			question: null, 
+			oldQuestion: null,
+			guess: '',
+			answered: false,
+			totalScore: 0,
 		};
-		this.api.getQuestion(0).then(res => this.setState({ question:res }));
+		this.api.getQuestion(this.state.id).then(res => this.setState({ question: res }));
 	}
 
-	onEnter(){
-		this.setState({answered:true})
+	onEnter() {
+		if (this.state.answered) {
+			this.setState({ totalScore: this.state.totalScore + this.state.score, oldQuestion:this.state.question });
+			this.api
+				.getQuestion(this.state.id + 1)
+				.then(res => this.setState({ question: res, id: this.state.id + 1, answered: false, score: 9999999 }));
+		} else {
+			this.setState({
+				score:
+					Math.abs(Math.log(this.state.question.a) - Math.log(this.state.guess)) /
+					Math.log(this.state.question.a),
+				answered: true,
+			});
+		}
 	}
 
 	render() {
-
 		return (
-			<React.Fragment>
-				<h1>STATBOY</h1>
-				<h2 className="text-left">>{this.state.question?this.state.question.q:""}</h2>
-				<h3 className="text-left">({this.state.question?this.state.question.unit:""})</h3>
-				<input onKeyPress={(e)=>e.keyCode||e.charCode==13?this.onEnter():""}   onChange={e=>this.setState({guess:e.target.value})}></input>
-				<h3 className="text-center">{this.state.answered?this.state.question.a:"press enter"} </h3>
-				<h3 className="text-left">score:{this.state.answered?Math.log10(Math.abs((Number(this.state.question.a)-Number(this.state.guess)))):"---"} </h3>
-				
-			</React.Fragment>
+			<div className="row ">
+				<div className="col border-bottom">
+					<h1 className="text-center">STATBOY</h1>
+				</div>
+				<div className="w-100" />
+				<div className="col ">
+					<div className="mt-5 ml-5">
+						{this.state.question != null ? (
+							<QuestionHolder key={this.state.question.id} question={this.state.question} oldQuestion={this.state.oldQuestion} />
+						) : (
+							''
+						)}
+					</div>
+				</div>
+				<div className="w-100" />
+				<div className="col text-center">
+					<input 
+						onKeyPress={e => (e.keyCode || e.charCode == 13 ? this.onEnter() : '')}
+						onChange={e => this.setState({ guess: e.target.value })}
+					/>
+				</div>
+				<div className="w-100" />
+				<div className="col">
+					<h3 className="text-center">{this.state.answered ? this.state.question.a : 'press enter'} </h3>
+				</div>
+				<div className="w-100" />
+				<div className="col">
+					<h3 className="text-left ml-5">
+						score:
+						{this.state.answered ? this.state.score.toFixed(4) : '-'}
+					</h3>
+				</div>
+				<div className="w-100" />
+				<div className="col">
+					<h3 className="text-left ml-5">
+						total score:
+						{this.state.totalScore.toFixed(4)}
+					</h3>
+				</div>
+			</div>
 		);
 	}
 }
-
-
 
 export default App;
---
+++ client/src/QuestionDisplay/QuestionDisplay.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class QuestionDisplay extends Component {
+	render() {
+		return (
+            this.props.question?
+			<div >
+				<h2>{this.props.question.q}</h2>
+				<h3 >{this.props.question.unit}</h3>
+			</div>
+            :""
+		);
+	}
+}
+
+export default QuestionDisplay;
