257cbddea5c6ca053eb1bef4b5e09184b9f21785
Erland <nicolasjonason@gmail.com>
2019-02-22 12:22:55 +0100

    add history questions

---
+++ scraping/scraper.py
@@ -0,0 +1,73 @@
+# import libraries
+from bs4 import BeautifulSoup,Tag, NavigableString
+import requests
+from functools import reduce
+import time
+
+
+
+def parseDateList(ul):
+    out=[]
+    for day in ul.contents:
+        # print(day)
+        str=""
+        if not isinstance(day,NavigableString):
+            for tag in day.contents:
+                if tag.name=="a":
+                   str+=tag["title"]
+                elif tag.name=="ul":
+                    out=out+[str+e for e in parseDate(tag)]
+                    str=""
+                elif isinstance(tag.string,NavigableString):
+                    str+=tag.string
+            out.append(str)
+    return out
+
+def parseDate(ul):
+    out=[]
+    for ev in ul.contents:
+        if not isinstance(ev,NavigableString):
+            str=""
+            for tag in ev.contents:
+                if tag.name=="a":
+                        str+=tag["title"]
+                elif isinstance(tag.string,NavigableString):
+                        str+=tag.string
+            out.append(str)
+    return out
+
+id=0
+f = open("Dates/events.json".format(), "w")
+f.write("dates={")
+for d in range(1850,1859): 
+
+    print("scraping article for year {}".format(d))
+    
+
+    url = "https://en.wikipedia.org/wiki/{}".format(d)
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    el = soup.find("span", attrs={"id": "Events"}).parent
+
+    try:
+        events=[]
+        while True:
+            el=el.next_sibling
+            if el.name=="h2":
+                break
+            if el.name == "ul":
+                m=parseDateList(el)
+                m=[o.replace("\n"," - ") for o in m]
+                events=events+list(filter(lambda x: len(x)>3, m))
+            
+        for e in events:
+            f.write("\n{{\"id\": {}, \"q\": \"{}\",\"unit\": \"year\", \"a\": {} }},".format(id,e.replace("\"","\'"),d))
+            id=id+1
+        time.sleep(1)
+    
+    except:
+        print("Error while scraping article for year {}".format(d))
+        pass
+
+f.write("}")
